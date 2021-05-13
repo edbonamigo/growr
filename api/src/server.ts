2853f5d7f641551aa7e41cc86d053c6370c151d8
edbmuller <edbmuller@gmail.com>
2021-05-13 00:17:38 -0300

    FEAT: typeorm and database connection

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express from 'express';
+
+import './database';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "ok" });
+app.get('/', (request, response) => {
+  return response.json({ message: 'ok' });
 });
 
 app.listen(3333, () => {
-  console.log("🚀 Server started on port 3333");
+  console.log('🚀 Server started on port 3333');
 });
