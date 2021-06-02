bcb643481c7a8a2ec0d09189c4f93bf4c9dad819
edbmuller <edbmuller@gmail.com>
2021-06-02 20:13:37 -0300

    FEAT: Abstracting routes from server file

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 import './database';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'ok' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
