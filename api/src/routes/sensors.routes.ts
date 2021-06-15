58a88c6d7ba97fa7091083a28aae05e9a11a37c9
edbmuller <edbmuller@gmail.com>
2021-06-15 18:50:04 -0300

    :hammer: Add Sensor routes GET and POST

--- api/src/routes/sensors.routes.ts
+++ api/src/routes/sensors.routes.ts
@@ -1,9 +1,27 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+
+import Sensor from '../entities/Sensor';
+import CreateSensorService from '../services/CreateSensorService';
 
 const sensorsRouter = Router();
 
-sensorsRouter.get('/', (request, response) => {
-  return response.json({ message: 'ok' });
+sensorsRouter.get('/', async (request, response) => {
+  const sensorsRepository = getRepository(Sensor);
+
+  const sensors = await sensorsRepository.find();
+
+  return response.json(sensors);
+});
+
+sensorsRouter.post('/', async (request, response) => {
+  const { name, unity } = request.body;
+
+  const createSensor = new CreateSensorService();
+
+  const sensor = await createSensor.execute({ name, unity });
+
+  return response.json({ sensor });
 });
 
 export default sensorsRouter;
